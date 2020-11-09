06b1ce515df7cbcfcf44426ef3e6131ab3014d52
santicr <44589626+santicr@users.noreply.github.com>
2020-11-08 20:23:16 -0500

    Responsive Animal Details
    Responsive Animal Details, cuando se voltea hay cosas que se deben arreglar (ver otro día)

--- pages/AnimalDetails.jsx
+++ pages/AnimalDetails.jsx
@@ -6,7 +6,7 @@ import styles from './AnimalDetails.css';
 class AnimalDetails extends Component {
   render() {
     return(
-      <div>
+      <div id = "escritorio">
         <div class="card">
           <img id = "an" class="card-img-top" src={this.props.animal.img} alt="Card image cap"/>
           <div class="card-body">
